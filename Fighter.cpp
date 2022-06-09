151546cc5fa11b0fca57cdcde6c088661c49df6c
ShlomoAvn <shlomo.avnon@campus.technion.ac.il>
2022-06-09 14:17:23 +0300

    Fighter

---
+++ Fighter.cpp
@@ -0,0 +1,12 @@
+//
+// Created by teich on 09/06/2022.
+//
+
+#include "Fighter.h"
+
+int Fighter::getAttackStrength() const {
+    return m_level + 2*m_force;
+}//
+// Created by shlom on 09/06/2022.
+//
+
---
+++ Fighter.h
@@ -0,0 +1,19 @@
+//
+// Created by teich on 09/06/2022.
+//
+
+#include "Player.h"
+
+
+#ifndef PLAYER_CPP_FIGHTER_H
+#define PLAYER_CPP_FIGHTER_H
+
+class Fighter : public Player{
+public:
+    Fighter(const char* name, int hp = MAX_HP, int force = STARTING_FORCE);
+    int getAttackStrength() const override;
+    ~Fighter()=default;
+
+};
+
+#endif //PLAYER_CPP_FIGHTER_H
