ee206956a3e7417eacd71a4b947e521ea69664e0
teich <roi.teichman@campus.technion.ac.il>
2022-06-09 17:03:44 +0300

    Pitfall empty

---
+++ Cards/Pitfall.cpp
@@ -0,0 +1,4 @@
+//
+// Created by teich on 09/06/2022.
+//
+
---
+++ Cards/Pitfall.h
@@ -0,0 +1,8 @@
+//
+// Created by teich on 09/06/2022.
+//
+
+#ifndef MTMCHKIN_H_PITFALL_H
+#define MTMCHKIN_H_PITFALL_H
+
+#endif //MTMCHKIN_H_PITFALL_H
