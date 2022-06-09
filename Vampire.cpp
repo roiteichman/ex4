42f61668d740fae1c2fbb8eb63291a3be2aa518c
teich <roi.teichman@campus.technion.ac.il>
2022-06-09 15:06:02 +0300

    Vampire

--- Vampire.cpp
+++ Vampire.cpp
@@ -2,3 +2,4 @@
 // Created by shlom on 09/06/2022.
 //
 
+
--- Vampire.h
+++ Vampire.h
@@ -1,6 +1,7 @@
 //
 // Created by shlom on 09/06/2022.
 //
+#include "Card.h"
 
 #ifndef PLAYER_CPP_VAMPIRE_H
 #define PLAYER_CPP_VAMPIRE_H
