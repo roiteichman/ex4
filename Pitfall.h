191f35ac9e17a64763ca4b60fba5f3a0dbf19ffa
teich <roi.teichman@campus.technion.ac.il>
2022-06-09 17:24:27 +0300

    Changes by shlomo_

--- Pitfall.h
+++ Pitfall.h
@@ -6,9 +6,20 @@
 #ifndef MTMCHKIN_H_PITFALL_H
 #define MTMCHKIN_H_PITFALL_H
 
-class Pitfall : public Card {
-    Pitfall();
-    ~Pitfall() = default;
-}
+const int P_FORCE=10;
+const int P_HP_LOSS=10;
+const int P_LOOT=2;
 
+class Pitfall : public Card{
+public:
+    Pitfall();
+    ~Pitfall() override =default;
+
+protected:
+    void print(ostream &os) const override;
+
+
+private:
+    CardStats m_stats = CardStats(P_FORCE, P_HP_LOSS, IRRELEVANT, IRRELEVANT, IRRELEVANT, P_LOOT);
+};
 #endif //MTMCHKIN_H_PITFALL_H
