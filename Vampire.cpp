9997387f22d0344b12bddaf549d119f81c31da4f
teich <roi.teichman@campus.technion.ac.il>
2022-06-09 15:06:22 +0300

    Vampire

--- Vampire.cpp
+++ Vampire.cpp
@@ -1,5 +1,10 @@
 //
 // Created by shlom on 09/06/2022.
 //
+#include "Vampire.h"
+
+Vampire::Vampire():
+Card(CardType::Battle, m_stats)
+{}
 
 
--- Vampire.h
+++ Vampire.h
@@ -6,4 +6,18 @@
 #ifndef PLAYER_CPP_VAMPIRE_H
 #define PLAYER_CPP_VAMPIRE_H
 
+const int FORCE=10;
+const int HP_LOSS=10;
+const int LOOT=2;
+const int IRRELEVANT=0;
+
+class Vampire : public Card{
+public:
+    Vampire();
+    ~Vampire()=default;
+
+private:
+    CardStats m_stats = CardStats(FORCE, HP_LOSS, IRRELEVANT, IRRELEVANT, IRRELEVANT, LOOT);
+};
+
 #endif //PLAYER_CPP_VAMPIRE_H
